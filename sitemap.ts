85ccbcc0396fcf8c0027b4ca4b85b0a08e2803bd
indigo165e83 <indigo165e833@gmail.com>
2026-01-31 05:53:08 +0900

    fix: sitemap

--- sitemap.ts
+++ sitemap.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "https://free-images.indigo165e83.com"; // 本番URL
+  const baseUrl = "https://4testing.indigo165e83.com"; // 本番URL
 
   // 1. 静的ページ
   const staticPages = [
@@ -20,18 +20,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
   ];
 
-  // 2. 動的ページ（画像詳細ページ）をDBから取得
-  const images = await prisma.image.findMany({
-    select: { id: true, updatedAt: true },
-    take: 1000, // 多すぎる場合は制限
-  });
-
-  const dynamicPages = images.map((image) => ({
-    url: `${baseUrl}/image/${image.id}`,
-    lastModified: image.updatedAt,
-    changeFrequency: "weekly" as const,
-    priority: 0.8,
-  }));
-
-  return [...staticPages, ...dynamicPages];
+  return [...staticPages ];
 }
